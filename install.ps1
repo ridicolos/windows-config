0db45661eea3c2d66cc55fc524ed35a6c1599153
ridicolos <25656713+ridicolos@users.noreply.github.com>
2021-05-21 23:13:13 +0200

    inital commit

---
+++ install.ps1
@@ -0,0 +1,41 @@
+
+
+
+choco install -y firacode 
+choco install -y cascadiacode
+
+choco install -y vscode
+
+choco install -y wsl 
+choco install wsl-ubuntu-1804
+
+choco install -y checksum
+
+choco install -y rufus
+choco install -y irfanview
+
+choco install -y powertoys
+choco install -y 7zip.install
+choco install -y ditto
+choco install -y greenshot
+choco install -y vlc
+choco install -y youtube-dl
+choco install -y lockhunter
+choco install -y teracopy
+choco install -y microsoft-windows-terminal
+choco install -y dbeaver
+choco install -y notepadplusplus
+choco install -y handbrake
+choco install -y ffmpeg
+choco install -y irfanviewplugins
+
+
+
+choco install -y firefox
+choco install -y googlechrome
+
+
+choco install -y taskbar-winconfig --params "'/LOCATION:top /SIZE:small /LOCKED:yes /COMBINED:no /CORTANA:icon /AUTOTRAY:no /USEPOWERSHELL:no'"
+choco install -y desktopicons-winconfig --params "'/Desktop:YES /UserFiles:YES /ControlPanel:NO /Network:NO /RecycleBin:YES /OneDrive:NO'"
+choco install -y explorer-winconfig --params "'/SHOWEXTENSIONS:yes /SHOWFULLPATH:yes /SHOWENCRYPTED:yes /SHOWCHECKBOXES:no /USESHARINGWIZARD:no /USEVIEW:details'"
+
